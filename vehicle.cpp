e9902c96f9a9c7f84df8ff32399a7dc776605871
Vishal Pathak <pathakvishal7310@gmail.com>
2026-04-29 16:35:44 +0530

    Refactor vehicle type handling and add utility functions

--- vehicle.cpp
+++ vehicle.cpp
@@ -7,7 +7,7 @@ Vehicle::Vehicle(int id, VehicleType vType)
     vehicleID = id;
     arrivalTime = time(0);
     waitingTime = 0;
-    type = vType;
+    vehicleCategory = vType;
 }
 
 Vehicle::Vehicle(int id, time_t arrival, VehicleType vType)
@@ -15,7 +15,12 @@ Vehicle::Vehicle(int id, time_t arrival, VehicleType vType)
     vehicleID = id;
     arrivalTime = arrival;
     waitingTime = 0;
-    type = vType;
+    vehicleCategory = vType;
+}
+
+int getOne()
+{
+    return 1;
 }
 
 int Vehicle::getVehicleID() const
@@ -35,27 +40,53 @@ int Vehicle::getWaitingTime() const
 
 VehicleType Vehicle::getType() const
 {
-    return type;
+    return vehicleCategory;
+}
+
+bool isVehicleTypeEmergency(VehicleType cat)
+{
+    if (cat == EMERGENCY)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool isVehicleTypePublicTransport(VehicleType cat)
+{
+    if (cat == PUBLIC_TRANSPORT)
+    {
+        return true;
+    }
+    return false;
 }
 
 bool Vehicle::isEmergency() const
 {
-    return type == EMERGENCY;
+    return isVehicleTypeEmergency(vehicleCategory);
 }
 
 bool Vehicle::isPublicTransport() const
 {
-    return type == PUBLIC_TRANSPORT;
+    return isVehicleTypePublicTransport(vehicleCategory);
 }
 
 int Vehicle::getPriorityMultiplier() const
 {
-    if (type == EMERGENCY)
-        return 10;
-    else if (type == PUBLIC_TRANSPORT)
-        return 2;
+    int multiplierValue;
+    if (vehicleCategory == EMERGENCY)
+    {
+        multiplierValue = 10;
+    }
+    else if (vehicleCategory == PUBLIC_TRANSPORT)
+    {
+        multiplierValue = 2;
+    }
     else
-        return 1;
+    {
+        multiplierValue = 1;
+    }
+    return multiplierValue;
 }
 
 void Vehicle::updateWaitingTime(int currentTime)
@@ -63,21 +94,28 @@ void Vehicle::updateWaitingTime(int currentTime)
     waitingTime = currentTime - arrivalTime;
 }
 
-string Vehicle::getTypeString() const 
+string Vehicle::getTypeString() const
 {
-    switch(type) {
-        case EMERGENCY: 
-            return "EMERGENCY";
-        case PUBLIC_TRANSPORT: 
-            return "BUS";
-        default: 
-            return "REGULAR";
+    string result;
+    if (vehicleCategory == EMERGENCY)
+    {
+        result = "EMERGENCY";
     }
+    else if (vehicleCategory == PUBLIC_TRANSPORT)
+    {
+        result = "BUS";
+    }
+    else
+    {
+        result = "REGULAR";
+    }
+    return result;
 }
 
 void Vehicle::displayVehicle() const
 {
-    cout << "Vehicle ID: " << vehicleID << ", Type: " << getTypeString() 
-         << ", Arrival Time: " << ctime(&arrivalTime) 
+    string typeLabel = getTypeString();
+    cout << "Vehicle ID: " << vehicleID << ", Type: " << typeLabel
+         << ", Arrival Time: " << ctime(&arrivalTime)
          << ", Waiting Time: " << waitingTime << " seconds" << endl;
 }
