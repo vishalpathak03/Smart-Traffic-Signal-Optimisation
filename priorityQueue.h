46059c8f4067829c65ec92e07ad0a3626e965364
Vishal Pathak <pathakvishal7310@gmail.com>
2026-04-29 16:31:02 +0530

    Refactor LanePriority and PriorityQueue structures

--- priorityQueue.h
+++ priorityQueue.h
@@ -6,35 +6,39 @@
 using namespace std;
 
 struct LanePriority {
-    Lane* lane;
-    double priority;
-    
-    LanePriority(Lane* l, double p) : lane(l), priority(p) {}
+    Lane* lanePtr;
+    double priorityValue;
+
+    LanePriority(Lane* l, double p)
+    {
+        lanePtr = l;
+        priorityValue = p;
+    }
 };
 
 class PriorityQueue {
 private:
-    vector<LanePriority> heap;
-    
-    void heapifyUp(int index);
-    void heapifyDown(int index);
-    int parent(int index);
-    int leftChild(int index);
-    int rightChild(int index);
-    void swap(int i, int j);
-    
+    vector<LanePriority> heapStorage;
+
+    void bubbleUp(int index);
+    void bubbleDown(int index);
+    int getParentIndex(int index);
+    int getLeftChildIndex(int index);
+    int getRightChildIndex(int index);
+    void swapEntries(int i, int j);
+
 public:
     PriorityQueue();
-    
+
     void insert(Lane* lane, double priority);
     Lane* extractMax();
     Lane* peekMax() const;
-    
+
     int size() const;
     bool isEmpty() const;
     void clear();
     void displayQueue() const;
-    
+
     void buildFromLanes(vector<Lane*>& lanes);
 };
 
