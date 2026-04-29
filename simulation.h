cb1ed94f7c825aa30e9588964a64d6903db3afd9
Vishal Pathak <pathakvishal7310@gmail.com>
2026-04-29 16:32:36 +0530

    Format simulation.h for consistency and readability

--- simulation.h
+++ simulation.h
@@ -16,44 +16,44 @@ private:
     TrafficSignal signal;
     PriorityQueue priorityQueue;
     TrafficPattern trafficPattern;
-    
+
     int currentTime;
     int totalCycles;
     int vehicleIDCounter;
-    
+
     int totalVehiclesProcessed;
     int totalVehiclesGenerated;
     int totalWaitingTime;
     double avgWaitingTime;
-    
+
     void generateVehicles();
     Lane* selectNextLane();
     void updateWaitingTimes();
-    
+
 public:
     Simulation();
     ~Simulation();
-    
+
     void initializeLanes();
     void setSignalTimings(int minGreen, int maxGreen, int yellow);
     void setTrafficPattern(TimeOfDay period);
-    
+
     void addEmergencyVehicle(string laneName);
     void blockLane(string laneName, double percentage);
-    
+
     void runCycle();
     void runSimulation(int numCycles);
-    
+
     void displayStatistics() const;
     void displayAllLanes() const;
     double getAverageWaitingTime() const;
     int getTotalProcessed() const;
-    
+
     void analyzeComplexity() const;
-    
+
     Lane* getLane(int index);
     Lane* getLane(string name);
-    
+
     void runFixedCycle(Lane* lane, int duration);
 };
 
